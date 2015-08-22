4f68d57c40de67fc3c55eddfecdfef3da9ea49c4
Yoad Snapir <yoadsn@users.noreply.github.com>
2015-08-22 23:12:21 +0300

    Start factoring out the generator to a separate project

---
+++ ActivityArrowDiagramGenerator/ActivityArrowDiagramGenerator.cs
@@ -0,0 +1,18 @@
+﻿using ActivityArrowDiagramGenerator.Model.Graph;
+using ActivityArrowDiagramGenerator.Model.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityArrowDiagramGenerator
+{
+    public class ActivityArrowDiagramGenerator
+    {
+        public ActivityArrowGraph GenerateGraph(ActivityProject project)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
